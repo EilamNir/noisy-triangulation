8338e3b31ecb74091158029da2b3daf0840e43d2
EilamNir <nir1eilam@gmail.com>
2023-04-19 15:06:21 +0300

    change parameter passing to support matlab 20

--- src/+simulation/Simulation.m
+++ src/+simulation/Simulation.m
@@ -19,12 +19,12 @@ import simulation.generate_path
 %% test straight lines and xy turns
 path1 = generate_path();
 % path1.TimeRes = 0.1;
-path1.add_straight_interval(20, theta=90, phi=45);
-path1.add_straight_interval(10, theta=80, phi=45);
-path1.add_xy_turn_interval(10, 5, override_theta=false);
+path1.add_straight_interval(20, "theta", 90, "phi", 45);
+path1.add_straight_interval(10, "theta", 80, "phi", 45);
+path1.add_xy_turn_interval(10, 5, "override_theta", false);
 path1.add_xy_turn_interval(10, -5);
-path1.add_xy_turn_interval(20, 15, override_theta=true, theta=100);
-path1.add_straight_interval(10, theta=90, phi=90);
+path1.add_xy_turn_interval(20, 15, "override_theta", true, "theta", 100);
+path1.add_straight_interval(10, "theta", 90, "phi", 90);
 
 figure
 hold on
@@ -37,7 +37,7 @@ plot3(path1.path(:,1), path1.path(:,2), path1.path(:,3), 'b.-');
 %% test 3d turns
 for i = [0, 45, 90]
     path2 = generate_path();
-    path2.add_straight_interval(20, theta=90, phi=45);
+    path2.add_straight_interval(20, "theta", 90, "phi", 45);
     path2.add_3d_turn_interval(20, 10, i)
     path2.add_straight_interval(30);
 
@@ -52,13 +52,13 @@ end
 % test sensors
 import simulation.noisy_sensor
 
-sensor1 = noisy_sensor(SensorPos, has_distance=true, has_angle=true, distance_noise_sigma=50);
+sensor1 = noisy_sensor(SensorPos, "has_distance", true, "has_angle", true, "distance_noise_sigma", 50);
 sensor2_pos = [1500,1600,150];
-sensor2 = noisy_sensor(sensor2_pos, has_distance=true, has_angle=true, distance_noise_sigma=50);
+sensor2 = noisy_sensor(sensor2_pos, "has_distance", true, "has_angle", true, "distance_noise_sigma", 50);
 sensor3_pos = [-310,10,0];
-sensor3 = noisy_sensor(sensor3_pos, has_distance=true, has_angle=true, phi_noise_sigma=1, theta_noise_sigma=1);
+sensor3 = noisy_sensor(sensor3_pos, "has_distance", true, "has_angle", true, "phi_noise_sigma", 1, "theta_noise_sigma", 1);
 sensor4_pos = [1510,1610,150];
-sensor4 = noisy_sensor(sensor4_pos, has_distance=true, has_angle=true, phi_noise_sigma=1, theta_noise_sigma=1);
+sensor4 = noisy_sensor(sensor4_pos, "has_distance", true, "has_angle", true, "phi_noise_sigma", 1, "theta_noise_sigma", 1);
 
 sensor_list = [sensor1, sensor2, sensor3, sensor4];
 color_list = ['r', 'g', 'y', 'c'];
