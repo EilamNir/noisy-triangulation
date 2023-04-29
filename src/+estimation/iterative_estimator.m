618c923d1d0e5a39681bb992df622a980e84e298
EilamNir <nir1eilam@gmail.com>
2023-04-29 22:54:19 +0300

    fix singular matrix problems

--- src/+estimation/iterative_estimator.m
+++ src/+estimation/iterative_estimator.m
@@ -2,6 +2,8 @@ classdef iterative_estimator < handle
     properties
         sensor_list
         last_location
+        d_s
+        Dd_s
     end
     methods
         function obj = iterative_estimator(sensor_list, initial_location_guess)
@@ -11,39 +13,46 @@ classdef iterative_estimator < handle
             end
             obj.sensor_list = sensor_list;
             obj.last_location = initial_location_guess;
-            % TODO: differentiate the functions in this init function and save symbolic functions for run time
+            
+            % Create x variable to differentiate
+            x = symmatrix('x', size(initial_location_guess));
+            
+            
+            % Create symbolic sensor location to let us differentiate once and not for every sensor
+            sens_loc = symmatrix('sens_loc', size(initial_location_guess));
+            % Symbolic function to differentiate
+            d = ((x-sens_loc) * (x-sens_loc).').^0.5;
+            % Create symbolic functions that can get parameters (matrix symbolic functions can't use parameters)
+            
+            d_s(symmatrix2sym(sens_loc), symmatrix2sym(x)) = symmatrix2sym(d);
+            Dd = diff(d,x);
+            Dd_s(symmatrix2sym(sens_loc), symmatrix2sym(x)) = symmatrix2sym(Dd);
+            obj.d_s = d_s;
+            obj.Dd_s = Dd_s;
         end
 
         function [dist, Ddist] = get_distances(obj, sensor_locations, x0)
             % when calculating by hand:
             % distances = ((x0(1) - sensor_locations(:,1)).^2 + (x0(2) - sensor_locations(:,2)).^2 + (x0(3) - sensor_locations(:,3)).^2).^0.5;
             % d_distances = (x0 - sensor_locations) ./ distances;
-            
+
             % Convert row to column vectors
             sensor_locations = sensor_locations';
             % Example of how vectors should look like:
             % sensor_locations = [0,0,0; 0,1,0; 1,0,0; -1,-1,0]';
             % x0 = [1, 1, 0];
 
-            % Create x variable to differentiate
-            x = symmatrix('x', size(x0));
+            x0_c = num2cell(x0);
+
             % Create location to put output vector of symbols
             f = zeros([1 size(sensor_locations, 2)]);
             Df = zeros([size(x0,2) size(sensor_locations, 2)]);
-            % Create symbolic sensor location to let us differentiate once and not for every sensor
-            sens_loc = symmatrix('sens_loc', size(x0));
-            % Symbolic function to differentiate
-            d = ((x-sens_loc) * (x-sens_loc).').^0.5;
-            % Create symbolic functions that can get parameters (matrix symbolic functions can't use parameters)
-            x0_c = num2cell(x0);
-            d_s(symmatrix2sym(sens_loc), symmatrix2sym(x)) = symmatrix2sym(d);
-            Dd = diff(d,x);
-            Dd_s(symmatrix2sym(sens_loc), symmatrix2sym(x)) = symmatrix2sym(Dd);
+            
             % Calculate function and derivative for each sensor
             for i = 1:length(f)
                 sens_loc_c = num2cell(sensor_locations(:,i));
-                f(i) = d_s(sens_loc_c{:}, x0_c{:});
-                Df(:,i) = Dd_s(sens_loc_c{:}, x0_c{:});
+                f(i) = obj.d_s(sens_loc_c{:}, x0_c{:});
+                Df(:,i) = obj.Dd_s(sens_loc_c{:}, x0_c{:});
             end
             % Convert vector of symbols to numbers
             dist = double(f)';
@@ -52,11 +61,49 @@ classdef iterative_estimator < handle
 
         function point = estimate_point_by_distances(obj, distances, sensor_locations, x0)
             current_estimate = x0';
+            last_estimate = current_estimate;
             y = distances;
-            for i = 1:3
+            for i = 1:5
                 [h_x0, H] = obj.get_distances(sensor_locations, current_estimate');
-                current_estimate = inv(H'*H)*H'*(y - h_x0 + H*current_estimate);
+                
+                % To calculate how close the H matrix is to being singular, uncomment this section
+                % [s, v, d] = svd(H'*H);
+                % a = diag(v);
+                % kappa = a(1)/a(end);
+                % display(kappa);
+                
+                % Add a small value to diagonal of H to prevent it from being singular
+                H = H + eye(size(H))*0.1;
+
+                % Make sure the matrix we use can be inverted
+                % (this is probably wasteful as it's probably quicker to just invert a 3x3 matrix in matlab than to test an if statement)
+                % if (size(H,1) == size(H,2))
+                %     Z = inv(H);
+                % else
+                %     Z = inv(H'*H)*H';
+                % end
+                % (the above is probably wasteful as it's probably quicker to just multiply a 3x3 matrix twice in matlab than to test an
+                % if statement, so let's just multiply)
+                Z = inv(H'*H)*H';
+                
+                % Get next estimate
+                current_estimate = Z*(y - h_x0 + H*current_estimate);
+                
+                % temp_estimate = inv(H + eye(size(H))*0.1)*(y-h_x0) + current_estimate;
+                % display(current_estimate);
+                % display(temp_estimate);
+                % display(current_estimate);
+
+                % current_estimate = temp_estimate;
+                if (norm(last_estimate - current_estimate) < 0.01)
+                    break;
+                end
+                last_estimate = current_estimate;
+
+                
             end
+            % loss = (h_x0 - distances)';
+            % display(loss);
             point = current_estimate';
         end
 
@@ -73,11 +120,14 @@ classdef iterative_estimator < handle
                 sensor_locations(i,:) = obj.sensor_list(i).sensor_position;
             end
             % estimated_path = obj.estimate_point_by_distances(all_distances(:,1), sensor_locations, obj.last_location);
+            f = waitbar(0, "please wait");
             for i = 1:size(all_distances, 2)
                 current_point = obj.estimate_point_by_distances(all_distances(:,i), sensor_locations, obj.last_location);
                 obj.last_location = current_point;
                 estimated_path(i,:) = current_point;
+                waitbar(i/size(all_distances, 2), f, "processing");
             end
+            close(f);
         end
     end
 end
--- src/+simulation/Simulation.m
+++ src/+simulation/Simulation.m
@@ -17,7 +17,7 @@ rng(42);
 import simulation.generate_path
 
 %% test straight lines and xy turns
-path1 = generate_path("initial_phi", 0, "initial_speed_xy", 50, "initial_speed_z", 10);
+path1 = generate_path("initial_phi", 0, "initial_speed_xy", 50, "initial_speed_z", 10, "initial_position", [0,0,1000], "TimeRes", 0.5);
 path1.add_straight_interval(10);
 path1.add_xy_turn_interval(10, -pi*3/(2 * 10));
 path1.add_straight_interval(10);
@@ -56,6 +56,8 @@ sensor_distance2_pos = [400, -7400, 0];
 sensor_distance2 = noisy_sensor(sensor_distance2_pos, "has_distance", true, "has_angle", false, "distance_noise_sigma", 15);
 sensor_distance3_pos = [800, 800, 0];
 sensor_distance3 = noisy_sensor(sensor_distance3_pos, "has_distance", true, "has_angle", false, "distance_noise_sigma", 15);
+sensor_distance4_pos = [-500, -500, 500];
+sensor_distance4 = noisy_sensor(sensor_distance4_pos, "has_distance", true, "has_angle", false, "distance_noise_sigma", 15);
 
 sensor_angle1_pos = [-5000,0,0];
 sensor_angle1 = noisy_sensor(sensor_angle1_pos, "has_distance", false, "has_angle", true, "theta_noise_sigma", 5, "theta_noise_sigma", 5);
@@ -66,94 +68,98 @@ sensor_angle3 = noisy_sensor(sensor_angle3_pos, "has_distance", false, "has_angl
 
 sensor_list = [sensor_distance1, sensor_distance2, sensor_distance3];
 % sensor_list = [sensor_angle1, sensor_angle2, sensor_angle3];
-color_list = ['r', 'g', 'y'];
+color_list = ['r', 'g', 'y', 'k'];
 
-figure
-hold on 
-grid minor
-view([-37.5 30]);
-xlim([min(path1.path(:,1))-10 max(path1.path(:,1))+10]);
-ylim([min(path1.path(:,2))-10 max(path1.path(:,2))+10]);
-zlim([min(path1.path(:,3))-10 max(path1.path(:,3))+10]);
-xlabel('x'); ylabel('y'); zlabel('z');
-plot3(path1.path(:,1), path1.path(:,2), path1.path(:,3), 'b.-');
+% figure
+% hold on 
+% grid minor
+% view([-37.5 30]);
+% xlim([min(path1.path(:,1))-10 max(path1.path(:,1))+10]);
+% ylim([min(path1.path(:,2))-10 max(path1.path(:,2))+10]);
+% zlim([min(path1.path(:,3))-10 max(path1.path(:,3))+10]);
+% xlabel('x'); ylabel('y'); zlabel('z');
+% plot3(path1.path(:,1), path1.path(:,2), path1.path(:,3), 'b.-');
 
 for i = 1:size(sensor_list, 2)
     sensor = sensor_list(i);
     color = color_list(i);
     sensor.calculate_measurements(path1.path);
-
-    % convert sensor output back to estimated positions
-    % import utils.matrix_helpers.TransposeMatrix3d
-    % noisy_positions = sensor.sensor_position + sensor.noisy_distances .* (TransposeMatrix3d(sensor.noisy_phis, sensor.noisy_thetas)');
-    % perfect_positions = sensor.sensor_position + sensor.perfect_distances .* (TransposeMatrix3d(sensor.perfect_phis, sensor.perfect_thetas)');
-
-    % scatter3(sensor.sensor_position(:,1), sensor.sensor_position(:,2), sensor.sensor_position(:,3), 'filled', color);
-    % plot3(noisy_positions(:,1), noisy_positions(:,2), noisy_positions(:,3), ['*' color]);
-    % plot3(perfect_positions(:,1), perfect_positions(:,2), perfect_positions(:,3), '^g');
 end
 
 %%
 % test iterative estimator
 import estimation.iterative_estimator;
-import simulation.noisy_sensor;
 
-sensor_distance1_pos = [0,0,1];
-sensor_distance1 = noisy_sensor(sensor_distance1_pos, "has_distance", true, "has_angle", false, "distance_noise_sigma", 0.1);
-sensor_distance2_pos = [1, 0, 0];
-sensor_distance2 = noisy_sensor(sensor_distance2_pos, "has_distance", true, "has_angle", false, "distance_noise_sigma", 0.1);
-sensor_distance3_pos = [0, 1, 0];
-sensor_distance3 = noisy_sensor(sensor_distance3_pos, "has_distance", true, "has_angle", false, "distance_noise_sigma", 0.1);
-sensor_list = [sensor_distance1, sensor_distance2, sensor_distance3];
-% sensor_list = [sensor_distance2, sensor_distance3];
-temp_path = [1.2,1,0; 0.95,0.99,0];
+it = iterative_estimator(sensor_list, path1.path(1,:));
+
+estimated_path = it.estimate_path_by_distance();
+
+%%
+% display path vs estimation
+figure
+hold on 
+grid minor
+view([-37.5 30]);
+%xlim([min(path1.path(:,1))-10 max(path1.path(:,1))+10]);
+%ylim([min(path1.path(:,2))-10 max(path1.path(:,2))+10]);
+%zlim([min(path1.path(:,3))-10 max(path1.path(:,3))+10]);
+xlabel('x'); ylabel('y'); zlabel('z');
+
 for i = 1:size(sensor_list, 2)
     sensor = sensor_list(i);
     color = color_list(i);
-    sensor.calculate_measurements(temp_path);
+    sensor.calculate_measurements(path1.path);
+    scatter3(sensor.sensor_position(:,1), sensor.sensor_position(:,2), sensor.sensor_position(:,3), 'filled', color);
 end
+plot3(path1.path(:,1), path1.path(:,2), path1.path(:,3), 'b.-');
 
-it = iterative_estimator(sensor_list, temp_path(1,:));
-
-estimated_path = it.estimate_path_by_distance();
-display(estimated_path);
+plot3(estimated_path(:,1), estimated_path(:,2), estimated_path(:,3), 'r*');
 
 %%
 % test estimator with working and non-working examples
 
-temp_path = [
-    1.0,1.0,0;
-    0.9,0.9,0;
-    0.8,0.8,0;
-    0.7,0.7,0;
-    0.6,0.6,0;
-    0.5,0.5,0;
-    0.4,0.4,0;
-    0.3,0.3,0;
-    0.2,0.2,0;
-    0.1,0.1,0;
-    0,0,0];
-% working
-sensor_pos = [0,0,1; 1,0,0; 0,1,0];
-get_path_from_sensor_pos(sensor_pos, temp_path);
-% non working
-temp_path = [1,1,0];
-sensor_pos = [0,0,0; 1,0,0; 0,1,0];
-get_path_from_sensor_pos(sensor_pos, temp_path);
+% temp_path = [
+%     1.0,1.0,0;
+%     0.9,0.9,0;
+%     0.8,0.8,0;
+%     0.7,0.7,0;
+%     0.6,0.6,0;
+%     0.5,0.5,0;
+%     0.4,0.4,0;
+%     0.3,0.3,0;
+%     0.2,0.2,0;
+%     0.1,0.1,0;
+%     0,0,0;
+%     ];
+% % working
+% sensor_pos = [0,0,1; 1,0,0; 0,1,0; 0,0.8,0];
+% get_path_from_sensor_pos(sensor_pos, temp_path);
+% % figure()
+% % plot3(sensor_pos(:,1),sensor_pos(:,2),sensor_pos(:,3));
+% % hold on;
+% % plot3(0.5,0.5,0, "r.");
+% % non working
+% temp_path = [
+%     0.5,0.5,0.5;
+%     0.5,0.5,0;
+%     0.5,0.5,-0.5;
+%     ];
+% %sensor_pos = [0,0,0; 1,0,0; 0,1,0];
+% %get_path_from_sensor_pos(sensor_pos, temp_path);
 
 %% functions
 
-function estimated_path = get_path_from_sensor_pos(sensor_pos, path)
-    import simulation.noisy_sensor;
-    import estimation.iterative_estimator;
-    sensor_list = noisy_sensor.empty();
-    for i = 1:size(sensor_pos, 1)
-        sensor = noisy_sensor(sensor_pos(i,:), "has_distance", true, "has_angle", false, "distance_noise_sigma", 0.1);
-        sensor_list(i) = sensor;
-        sensor.calculate_measurements(path);
-    end
+% function estimated_path = get_path_from_sensor_pos(sensor_pos, path)
+%     import simulation.noisy_sensor;
+%     import estimation.iterative_estimator;
+%     sensor_list = noisy_sensor.empty();
+%     for i = 1:size(sensor_pos, 1)
+%         sensor = noisy_sensor(sensor_pos(i,:), "has_distance", true, "has_angle", false, "distance_noise_sigma", 0.1);
+%         sensor_list(i) = sensor;
+%         sensor.calculate_measurements(path);
+%     end
 
-    it = iterative_estimator(sensor_list, path(1,:));
-    estimated_path = it.estimate_path_by_distance();
-    display(estimated_path);
-end
+%     it = iterative_estimator(sensor_list, path(1,:));
+%     estimated_path = it.estimate_path_by_distance();
+%     display(estimated_path);
+% end
