6aa2005a49ef93efa412beb9b3b7e7ec748b490f
DESKTOP-2JRMKFA\USER001 <danep95@gmail.com>
2023-04-29 20:08:39 +0300

    test non linear estimator

--- src/+simulation/Simulation.m
+++ src/+simulation/Simulation.m
@@ -1,159 +1,95 @@
 close all; clear; clc;
 %% simulation settings ****************************************************
-%SensorPos = [-300,0,0;-100 -100 -100];
-SensorPos = [-5000,0,0; 400, -7400, 0; 800, 800, 0];
+SensorPos = [-300,0,0;1700 -1100 10];
+% SensorPos = [-300,0,0;1700 -1100 10;2000 1000 50];
+% SensorPos = [-300,0,0;0 -100 0];
+% SensorPos = [-300,0,0];
 TargetPos = [0,0,0];
-TargetSpeed = 10;
-TargetRotSpeed = 5;
+TargetSpeed = 50;
+TargetRotSpeed = 10;
 TimeRes = 0.5;
 SimulationDuration = 150;
 TargetTheta = 90;
-TargetPhi = 45;
-
-% make the results reproducible by seeding the random number generator
-rng(42);
-
-%% generate a path
-import simulation.generate_path
-
-%% test straight lines and xy turns
-path1 = generate_path("initial_phi", 0, "initial_speed_xy", 50, "initial_speed_z", 10);
-path1.add_straight_interval(10);
-path1.add_xy_turn_interval(10, -pi*3/(2 * 10));
-path1.add_straight_interval(10);
+TargetPhi = 0;
 
 
+%% noise ******************************************************************
+DistanceNoiseForm = 'Normal';
+DistanceNoiseMu = 0;
+DistanceNoiseSigma = 10;
+
+AngleNoiseForm = 'Normal';
+AngleNoiseMu = 0;
+AngleNoiseSigma = 0;
+
+%% help function **********************************************************
+% TransposeMatrix = @(Phi, Theta) [cosd(Phi).*sind(Theta); sind(Phi).*sind(Theta); cosd(Theta)];
+TransposeMatrix = @(Phi) [cosd(Phi); sind(Phi)];
+%% main loop **************************************************************
+t = 0:TimeRes:SimulationDuration;
 figure
+scatter3(SensorPos(:,1), SensorPos(:,2), SensorPos(:,3), 'filled', 'r');
+xlabel('x'); ylabel('y'); zlabel('z');
 hold on
 grid minor
-view([-37.5 30]);
-xlabel('x'); ylabel('y'); zlabel('z');
-plot3(path1.path(:,1), path1.path(:,2), path1.path(:,3), 'b.-');
 
-
-
-% %% test 3d turns
-% for i = [0, 45, 90]
-%     path2 = generate_path();
-%     path2.add_straight_interval(20, "theta", 90, "phi", 0);
-%     path2.add_3d_turn_interval(20, 10, i)
-%     path2.add_straight_interval(30);
-
-%     figure
-%     hold on
-%     grid minor
-%     view([-37.5 30]);
-%     xlabel('x'); ylabel('y'); zlabel('z');
-%     plot3(path2.path(:,1), path2.path(:,2), path2.path(:,3), 'b.-');
-% end
-
-% % test sensors
-import simulation.noisy_sensor
-
-sensor_distance1_pos = [-5000,0,0];
-sensor_distance1 = noisy_sensor(sensor_distance1_pos, "has_distance", true, "has_angle", false, "distance_noise_sigma", 15);
-sensor_distance2_pos = [400, -7400, 0];
-sensor_distance2 = noisy_sensor(sensor_distance2_pos, "has_distance", true, "has_angle", false, "distance_noise_sigma", 15);
-sensor_distance3_pos = [800, 800, 0];
-sensor_distance3 = noisy_sensor(sensor_distance3_pos, "has_distance", true, "has_angle", false, "distance_noise_sigma", 15);
-
-sensor_angle1_pos = [-5000,0,0];
-sensor_angle1 = noisy_sensor(sensor_angle1_pos, "has_distance", false, "has_angle", true, "theta_noise_sigma", 5, "theta_noise_sigma", 5);
-sensor_angle2_pos = [400, -7400, 0];
-sensor_angle2 = noisy_sensor(sensor_angle2_pos, "has_distance", false, "has_angle", true, "theta_noise_sigma", 5, "theta_noise_sigma", 5);
-sensor_angle3_pos = [800, 800, 0];
-sensor_angle3 = noisy_sensor(sensor_angle3_pos, "has_distance", false, "has_angle", true, "theta_noise_sigma", 5, "theta_noise_sigma", 5);
-
-sensor_list = [sensor_distance1, sensor_distance2, sensor_distance3];
-% sensor_list = [sensor_angle1, sensor_angle2, sensor_angle3];
-color_list = ['r', 'g', 'y'];
-
-figure
-hold on 
-grid minor
-view([-37.5 30]);
-xlim([min(path1.path(:,1))-10 max(path1.path(:,1))+10]);
-ylim([min(path1.path(:,2))-10 max(path1.path(:,2))+10]);
-zlim([min(path1.path(:,3))-10 max(path1.path(:,3))+10]);
-xlabel('x'); ylabel('y'); zlabel('z');
-plot3(path1.path(:,1), path1.path(:,2), path1.path(:,3), 'b.-');
-
-for i = 1:size(sensor_list, 2)
-    sensor = sensor_list(i);
-    color = color_list(i);
-    sensor.calculate_measurements(path1.path);
-
-    % convert sensor output back to estimated positions
-    % import utils.matrix_helpers.TransposeMatrix3d
-    % noisy_positions = sensor.sensor_position + sensor.noisy_distances .* (TransposeMatrix3d(sensor.noisy_phis, sensor.noisy_thetas)');
-    % perfect_positions = sensor.sensor_position + sensor.perfect_distances .* (TransposeMatrix3d(sensor.perfect_phis, sensor.perfect_thetas)');
-
-    % scatter3(sensor.sensor_position(:,1), sensor.sensor_position(:,2), sensor.sensor_position(:,3), 'filled', color);
-    % plot3(noisy_positions(:,1), noisy_positions(:,2), noisy_positions(:,3), ['*' color]);
-    % plot3(perfect_positions(:,1), perfect_positions(:,2), perfect_positions(:,3), '^g');
-end
-
-%%
-% test iterative estimator
-import estimation.iterative_estimator;
-import simulation.noisy_sensor;
-
-sensor_distance1_pos = [0,0,1];
-sensor_distance1 = noisy_sensor(sensor_distance1_pos, "has_distance", true, "has_angle", false, "distance_noise_sigma", 0.1);
-sensor_distance2_pos = [1, 0, 0];
-sensor_distance2 = noisy_sensor(sensor_distance2_pos, "has_distance", true, "has_angle", false, "distance_noise_sigma", 0.1);
-sensor_distance3_pos = [0, 1, 0];
-sensor_distance3 = noisy_sensor(sensor_distance3_pos, "has_distance", true, "has_angle", false, "distance_noise_sigma", 0.1);
-sensor_list = [sensor_distance1, sensor_distance2, sensor_distance3];
-% sensor_list = [sensor_distance2, sensor_distance3];
-temp_path = [1.2,1,0; 0.95,0.99,0];
-for i = 1:size(sensor_list, 2)
-    sensor = sensor_list(i);
-    color = color_list(i);
-    sensor.calculate_measurements(temp_path);
-end
-
-it = iterative_estimator(sensor_list, temp_path(1,:));
-
-estimated_path = it.estimate_path_by_distance();
-display(estimated_path);
-
-%%
-% test estimator with working and non-working examples
-
-temp_path = [
-    1.0,1.0,0;
-    0.9,0.9,0;
-    0.8,0.8,0;
-    0.7,0.7,0;
-    0.6,0.6,0;
-    0.5,0.5,0;
-    0.4,0.4,0;
-    0.3,0.3,0;
-    0.2,0.2,0;
-    0.1,0.1,0;
-    0,0,0];
-% working
-sensor_pos = [0,0,1; 1,0,0; 0,1,0];
-get_path_from_sensor_pos(sensor_pos, temp_path);
-% non working
-temp_path = [1,1,0];
-sensor_pos = [0,0,0; 1,0,0; 0,1,0];
-get_path_from_sensor_pos(sensor_pos, temp_path);
-
-%% functions
-
-function estimated_path = get_path_from_sensor_pos(sensor_pos, path)
-    import simulation.noisy_sensor;
-    import estimation.iterative_estimator;
-    sensor_list = noisy_sensor.empty();
-    for i = 1:size(sensor_pos, 1)
-        sensor = noisy_sensor(sensor_pos(i,:), "has_distance", true, "has_angle", false, "distance_noise_sigma", 0.1);
-        sensor_list(i) = sensor;
-        sensor.calculate_measurements(path);
+PredictTargetPos = TargetPos.*ones(2,1);
+for i = t
+    if exist('ax1','var')                                                  % remove old plot
+        pause(0.1)
+        delete([ax1, ax2, ax4, ax5]);
+    end
+    ax1 = plot3(TargetPos(:,1), TargetPos(:,2), TargetPos(:,3), 'b');
+    ax2 = scatter3(TargetPos(end,1), TargetPos(end,2), TargetPos(end,3), 'filled', 'b');
+    
+    TargetPos(end+1,1:2) = TargetPos(end,1:2) + (TimeRes*TargetSpeed.*TransposeMatrix(TargetPhi))';
+    TargetPos(end,3) = TargetPos(end-1,3) + 10*TimeRes;
+    
+    % Target execute loop 
+    if i > 40 && i < 40+27.5
+       TargetPhi = TargetPhi + TimeRes*(TargetRotSpeed);
+    end
+    
+    % sensor prediction 
+    PredictDistance = vecnorm(TargetPos(end,:)-SensorPos,2,2) + random(DistanceNoiseForm, DistanceNoiseMu, DistanceNoiseSigma);
+%     PredictAnglePhi = atan2d(TargetPos(end,2)-SensorPos(:,2),TargetPos(end,1)-SensorPos(:,1)) + random(AngleNoiseForm, AngleNoiseMu, AngleNoiseSigma);
+%     PredictAngleTheta = atan2d(norm(TargetPos(end,1:2)-SensorPos(:,1:2)),TargetPos(end,3)-SensorPos(:,3)) + random(AngleNoiseForm, AngleNoiseMu, AngleNoiseSigma);
+%     PredictTargetPos = SensorPos + PredictDistance.*reshape(TransposeMatrix(PredictAnglePhi, PredictAngleTheta),[],3);
+    while true
+    %for j = 1:10
+        X0Distance = vecnorm(mean(PredictTargetPos)-SensorPos,2,2);
+        if abs(PredictDistance - X0Distance) < 20
+            break;
+        end
+        if(any(isnan(PredictTargetPos(:,1)./X0Distance + abs(PredictTargetPos(:,1)./X0Distance)<1),'all'))
+            return
+        end
+        disp(PredictTargetPos(:,1)./X0Distance + (abs(PredictTargetPos(:,1)./X0Distance)<0.5))
+        if any(isnan(PredictTargetPos(:,1)./X0Distance + (abs(PredictTargetPos(:,1)./X0Distance)<0.5)),'all')
+            return
+        end
+        PredictTargetPos(:,1) = PredictTargetPos(:,1) + sign(PredictTargetPos(:,1)-SensorPos(:,1)).*((PredictDistance - X0Distance)./(PredictTargetPos(:,1)./X0Distance + (abs(PredictTargetPos(:,1)./X0Distance)<0.5)));
+        X0Distance = vecnorm(mean(PredictTargetPos)-SensorPos,2,2);
+        if any(isnan(PredictTargetPos(:,2)./X0Distance + (abs(PredictTargetPos(:,2)./X0Distance)<0.5)),'all')
+            return
+        end
+        PredictTargetPos(:,2) = PredictTargetPos(:,2) + sign(PredictTargetPos(:,2)-SensorPos(:,2)).*((PredictDistance - X0Distance)./(PredictTargetPos(:,2)./X0Distance + (abs(PredictTargetPos(:,2)./X0Distance)<0.5)));
+        X0Distance = vecnorm(mean(PredictTargetPos)-SensorPos,2,2);
+        if any(isnan(PredictTargetPos(:,3)./X0Distance + (abs(PredictTargetPos(:,3)./X0Distance)<0.5)),'all')
+            return
+        end
+        PredictTargetPos(:,3) = PredictTargetPos(:,3) + sign(PredictTargetPos(:,3)-SensorPos(:,3)).*((PredictDistance - X0Distance)./(PredictTargetPos(:,3)./X0Distance + (abs(PredictTargetPos(:,3)./X0Distance)<0.5)));
     end
 
-    it = iterative_estimator(sensor_list, path(1,:));
-    estimated_path = it.estimate_path_by_distance();
-    display(estimated_path);
+
+    ax3 = plot3(mean(PredictTargetPos(:,1)), mean(PredictTargetPos(:,2)), mean(PredictTargetPos(:,3)), '*k'); 
+    [X,Y,Z] = sphere;
+    
+    ax4 = surf(X*PredictDistance(1)+SensorPos(1,1),Y*PredictDistance(1)+SensorPos(1,2),Z*PredictDistance(1)+SensorPos(1,3),...
+        'EdgeColor','none','FaceAlpha',0.2,'FaceColor','red');
+    ax5 = surf(X*PredictDistance(2)+SensorPos(2,1),Y*PredictDistance(2)+SensorPos(2,2),Z*PredictDistance(2)+SensorPos(2,3),...
+        'EdgeColor','none','FaceAlpha',0.2,'FaceColor','red');
+%     ax6 = surf(X*PredictDistance(3)+SensorPos(3,1),Y*PredictDistance(3)+SensorPos(3,2),Z*PredictDistance(3)+SensorPos(3,3),...
+%         'EdgeColor','none','FaceAlpha',0.2,'FaceColor','red');
+    axis equal
 end
