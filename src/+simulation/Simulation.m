a0c90bfa9ac1a85633aa461ca3bf1074ba2f1ef7
DESKTOP-2JRMKFA\USER001 <danep95@gmail.com>
2023-04-30 23:27:33 +0300

    update target position estimator

--- src/+simulation/Simulation.m
+++ src/+simulation/Simulation.m
@@ -1,7 +1,7 @@
 close all; clear; clc;
 %% simulation settings ****************************************************
-SensorPos = [-300,0,0;1700 -1100 10];
-% SensorPos = [-300,0,0;1700 -1100 10;2000 1000 50];
+% SensorPos = [-300,0,0;1700 -1100 10];
+SensorPos = [-5000,0,0;1700 -7000 0;2000 1000 0];
 % SensorPos = [-300,0,0;0 -100 0];
 % SensorPos = [-300,0,0];
 TargetPos = [0,0,0];
@@ -25,6 +25,7 @@ AngleNoiseSigma = 0;
 %% help function **********************************************************
 % TransposeMatrix = @(Phi, Theta) [cosd(Phi).*sind(Theta); sind(Phi).*sind(Theta); cosd(Theta)];
 TransposeMatrix = @(Phi) [cosd(Phi); sind(Phi)];
+H = @(x) (x)./vecnorm(x,2,2);
 %% main loop **************************************************************
 t = 0:TimeRes:SimulationDuration;
 figure
@@ -33,11 +34,11 @@ xlabel('x'); ylabel('y'); zlabel('z');
 hold on
 grid minor
 
-PredictTargetPos = TargetPos.*ones(2,1);
+PredictTargetPos = TargetPos;
 for i = t
     if exist('ax1','var')                                                  % remove old plot
         pause(0.1)
-        delete([ax1, ax2, ax4, ax5]);
+        delete([ax1, ax2, ax4, ax5, ax6]);
     end
     ax1 = plot3(TargetPos(:,1), TargetPos(:,2), TargetPos(:,3), 'b');
     ax2 = scatter3(TargetPos(end,1), TargetPos(end,2), TargetPos(end,3), 'filled', 'b');
@@ -55,30 +56,20 @@ for i = t
 %     PredictAnglePhi = atan2d(TargetPos(end,2)-SensorPos(:,2),TargetPos(end,1)-SensorPos(:,1)) + random(AngleNoiseForm, AngleNoiseMu, AngleNoiseSigma);
 %     PredictAngleTheta = atan2d(norm(TargetPos(end,1:2)-SensorPos(:,1:2)),TargetPos(end,3)-SensorPos(:,3)) + random(AngleNoiseForm, AngleNoiseMu, AngleNoiseSigma);
 %     PredictTargetPos = SensorPos + PredictDistance.*reshape(TransposeMatrix(PredictAnglePhi, PredictAngleTheta),[],3);
-    while true
-    %for j = 1:10
-        X0Distance = vecnorm(mean(PredictTargetPos)-SensorPos,2,2);
+%     while true
+    for j = 1:10
+        X0Distance = vecnorm(PredictTargetPos-SensorPos,2,2);
         if abs(PredictDistance - X0Distance) < 20
             break;
         end
-        if(any(isnan(PredictTargetPos(:,1)./X0Distance + abs(PredictTargetPos(:,1)./X0Distance)<1),'all'))
-            return
-        end
-        disp(PredictTargetPos(:,1)./X0Distance + (abs(PredictTargetPos(:,1)./X0Distance)<0.5))
-        if any(isnan(PredictTargetPos(:,1)./X0Distance + (abs(PredictTargetPos(:,1)./X0Distance)<0.5)),'all')
-            return
-        end
-        PredictTargetPos(:,1) = PredictTargetPos(:,1) + sign(PredictTargetPos(:,1)-SensorPos(:,1)).*((PredictDistance - X0Distance)./(PredictTargetPos(:,1)./X0Distance + (abs(PredictTargetPos(:,1)./X0Distance)<0.5)));
-        X0Distance = vecnorm(mean(PredictTargetPos)-SensorPos,2,2);
-        if any(isnan(PredictTargetPos(:,2)./X0Distance + (abs(PredictTargetPos(:,2)./X0Distance)<0.5)),'all')
-            return
-        end
-        PredictTargetPos(:,2) = PredictTargetPos(:,2) + sign(PredictTargetPos(:,2)-SensorPos(:,2)).*((PredictDistance - X0Distance)./(PredictTargetPos(:,2)./X0Distance + (abs(PredictTargetPos(:,2)./X0Distance)<0.5)));
-        X0Distance = vecnorm(mean(PredictTargetPos)-SensorPos,2,2);
-        if any(isnan(PredictTargetPos(:,3)./X0Distance + (abs(PredictTargetPos(:,3)./X0Distance)<0.5)),'all')
-            return
-        end
-        PredictTargetPos(:,3) = PredictTargetPos(:,3) + sign(PredictTargetPos(:,3)-SensorPos(:,3)).*((PredictDistance - X0Distance)./(PredictTargetPos(:,3)./X0Distance + (abs(PredictTargetPos(:,3)./X0Distance)<0.5)));
+        PredictTargetPos = PredictTargetPos + (inv(H(PredictTargetPos-SensorPos)+eye(3,3)*0.1)*(PredictDistance-X0Distance))';
+        
+%         disp(PredictTargetPos(:,1)./X0Distance + (abs(PredictTargetPos(:,1)./X0Distance)<0.5))
+%         PredictTargetPos(:,1) = PredictTargetPos(:,1) + sign(PredictTargetPos(:,1)-SensorPos(:,1)).*((PredictDistance - X0Distance)./(PredictTargetPos(:,1)./X0Distance + (abs(PredictTargetPos(:,1)./X0Distance)<0.5)));
+%         X0Distance = vecnorm(mean(PredictTargetPos)-SensorPos,2,2);
+%         PredictTargetPos(:,2) = PredictTargetPos(:,2) + sign(PredictTargetPos(:,2)-SensorPos(:,2)).*((PredictDistance - X0Distance)./(PredictTargetPos(:,2)./X0Distance + (abs(PredictTargetPos(:,2)./X0Distance)<0.5)));
+%         X0Distance = vecnorm(mean(PredictTargetPos)-SensorPos,2,2);
+%         PredictTargetPos(:,3) = PredictTargetPos(:,3) + sign(PredictTargetPos(:,3)-SensorPos(:,3)).*((PredictDistance - X0Distance)./(PredictTargetPos(:,3)./X0Distance + (abs(PredictTargetPos(:,3)./X0Distance)<0.5)));
     end
 
 
@@ -89,7 +80,7 @@ for i = t
         'EdgeColor','none','FaceAlpha',0.2,'FaceColor','red');
     ax5 = surf(X*PredictDistance(2)+SensorPos(2,1),Y*PredictDistance(2)+SensorPos(2,2),Z*PredictDistance(2)+SensorPos(2,3),...
         'EdgeColor','none','FaceAlpha',0.2,'FaceColor','red');
-%     ax6 = surf(X*PredictDistance(3)+SensorPos(3,1),Y*PredictDistance(3)+SensorPos(3,2),Z*PredictDistance(3)+SensorPos(3,3),...
-%         'EdgeColor','none','FaceAlpha',0.2,'FaceColor','red');
+    ax6 = surf(X*PredictDistance(3)+SensorPos(3,1),Y*PredictDistance(3)+SensorPos(3,2),Z*PredictDistance(3)+SensorPos(3,3),...
+        'EdgeColor','none','FaceAlpha',0.2,'FaceColor','red');
     axis equal
 end
