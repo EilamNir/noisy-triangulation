fad68def831e909fd2e45c504c496fbc04b4190a
EilamNir <nir1eilam@gmail.com>
2023-05-01 00:59:37 +0300

    use path generated from generate_path lib

--- src/+simulation/Simulation.m
+++ src/+simulation/Simulation.m
@@ -1,7 +1,8 @@
 close all; clear; clc;
 %% simulation settings ****************************************************
 % SensorPos = [-300,0,0;1700 -1100 10];
-SensorPos = [-5000,0,0;1700 -7000 0;2000 1000 0];
+% SensorPos = [-5000,0,0;1700 -7000 0;2000 1000 0];
+SensorPos = [-5000,0,0;400 -7400 0;800 800 0];
 % SensorPos = [-300,0,0;0 -100 0];
 % SensorPos = [-300,0,0];
 TargetPos = [0,0,0];
@@ -12,6 +13,21 @@ SimulationDuration = 150;
 TargetTheta = 90;
 TargetPhi = 0;
 
+
+% make the results reproducible by seeding the random number generator
+rng(42);
+
+%% generate a path
+import simulation.generate_path
+
+%% test straight lines and xy turns
+path1 = generate_path("initial_phi", 0, "initial_speed_xy", 50, "initial_speed_z", 10, "initial_position", [0,0,0], "TimeRes", 0.1);
+path1.add_straight_interval(10);
+path1.add_xy_turn_interval(10, -pi*3/(2 * 10));
+path1.add_straight_interval(10);
+
+true_path = path1.path;
+targetPos = true_path(1);
 
 %% noise ******************************************************************
 DistanceNoiseForm = 'Normal';
@@ -27,7 +43,7 @@ AngleNoiseSigma = 0;
 TransposeMatrix = @(Phi) [cosd(Phi); sind(Phi)];
 H = @(x) (x)./vecnorm(x,2,2);
 %% main loop **************************************************************
-t = 0:TimeRes:SimulationDuration;
+% t = 0:TimeRes:SimulationDuration;
 figure
 scatter3(SensorPos(:,1), SensorPos(:,2), SensorPos(:,3), 'filled', 'r');
 xlabel('x'); ylabel('y'); zlabel('z');
@@ -35,7 +51,7 @@ hold on
 grid minor
 
 PredictTargetPos = TargetPos;
-for i = t
+for i = 1:(size(true_path, 1) - 1)
     if exist('ax1','var')                                                  % remove old plot
         pause(0.1)
         delete([ax1, ax2, ax4, ax5, ax6]);
@@ -43,13 +59,14 @@ for i = t
     ax1 = plot3(TargetPos(:,1), TargetPos(:,2), TargetPos(:,3), 'b');
     ax2 = scatter3(TargetPos(end,1), TargetPos(end,2), TargetPos(end,3), 'filled', 'b');
     
-    TargetPos(end+1,1:2) = TargetPos(end,1:2) + (TimeRes*TargetSpeed.*TransposeMatrix(TargetPhi))';
-    TargetPos(end,3) = TargetPos(end-1,3) + 10*TimeRes;
+    TargetPos(end+1,:) = true_path(i + 1, :);
+    % TargetPos(end+1,1:2) = TargetPos(end,1:2) + (TimeRes*TargetSpeed.*TransposeMatrix(TargetPhi))';
+    % TargetPos(end,3) = TargetPos(end-1,3) + 10*TimeRes;
     
-    % Target execute loop 
-    if i > 40 && i < 40+27.5
-       TargetPhi = TargetPhi + TimeRes*(TargetRotSpeed);
-    end
+    % % Target execute loop 
+    % if i > 40 && i < 40+27.5
+    %    TargetPhi = TargetPhi + TimeRes*(TargetRotSpeed);
+    % end
     
     % sensor prediction 
     PredictDistance = vecnorm(TargetPos(end,:)-SensorPos,2,2) + random(DistanceNoiseForm, DistanceNoiseMu, DistanceNoiseSigma);
